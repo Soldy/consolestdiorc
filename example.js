e242d026dc20e5fe2ac9c7a765d4376a7999878e
Soldy <4786022+Soldy@users.noreply.github.com>
2021-02-05 20:38:40 +0000

    simple example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ example.js
@@ -0,0 +1,10 @@
+const stdio = new (require('./index.js')).base();
+
+stdio.clear();
+stdio.printLn('test cool');
+stdio.printLn('test cool');
+stdio.printLn('test cool');
+stdio.printLn('test cool');
+stdio.printLn('test cool');
+stdio.printTo('first line',1,1);
+stdio.cursorTo();
